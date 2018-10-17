e8ed31cc3e82d9c35a171983295b756208e413f3
Michael Seery <michaelseery@outlook.com.au>
2018-10-17 16:21:45 +1100

    End of Section 12

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -27,6 +27,7 @@ namespace DatingApp.API.Helpers
             CreateMap<userForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
